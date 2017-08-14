a4db4889556899968dcdc740f0609f867607c54b
moghbaie <moghbaie@stevens.edu>
2017-08-14 15:30:23 -0400

    update complexportal

--- complexPortaldb.py
+++ complexPortaldb.py
@@ -4,11 +4,32 @@ Created on Fri Aug 11 00:40:21 2017
 
 @author: moghb
 """
-
+import json
+import requests
+import logging
 from bioservices import IntactComplex
+
+your_project_directory="C:\\Users\\moghb\\OneDrive\\Documents\\Database\\expert_schema"
+
 s = IntactComplex()
-s.search('ndc80')
-s.search('ndc80', first=0, number=10,
-    filters='species_f:("Homo sapiens")',
-    facets='ptype_f')
-s.search('GO:0016491', first=10, number=10)
+r=s.search('nup53')['elements'][0]['complexAC']
+
+logger = logging.getLogger(__name__)
+url ="https://www.ebi.ac.uk/intact/complex-ws/details/"+r+"?format=json"
+
+def do_request( ):
+    resp = requests.get(url)
+    logger.debug('Requested {}'.format(resp.url))
+
+    if resp.status_code == 200:
+        return resp
+    else:
+        raise Exception(resp)
+
+j = json.loads(do_request().text)
+
+with open(os.path.join(your_project_directory, 'data.txt'), 'w') as outfile:
+    json.dump(j, outfile)
+    
+list(j["systematicName"].split(":"))
+
